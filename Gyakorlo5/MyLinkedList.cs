86b91a46767ff4976f924e536363b0bcb19329bf
Ektora <zsoltlucski@gmail.com>
2022-09-11 08:20:42 +0200

    A kezdeti hibás kód inicializálva

---
+++ Gyakorlo5/MyLinkedList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinkedList
+{
+    class MyLinkedList
+    {
+        public int Size { get; private set; }
+
+        Node Head { get; set; }
+
+        class Node
+        {
+            public int Value { get; private set; }
+            public Node Next { get; set; }
+
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(Value);
+                if (Next != null)
+                {
+                    sb.Append(" ").Append(Next);
+                }
+                return sb.ToString();
+            }
+
+            public Node(int val)
+            {
+                Value = val;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            // Write your code here
+        }
+
+        public bool AddFirst(int val)
+        {
+            // Write your code here
+        }
+
+        public bool AddLast(int val)
+        {
+            // Write your code here
+        }
+
+        public bool RemoveFirst()
+        {
+            // Write your code here
+        }
+
+        public bool RemoveLast()
+        {
+            // Write your code here
+        }
+
+        public bool RemoveFirstOccurrence(int val)
+        {
+            // Write your code here
+        }
+
+        public bool RemoveAll(int val)
+        {
+            // Write your code here
+        }
+
+        public bool Contains(int val)
+        {
+            // Write your code here
+        }
+
+        public override string ToString()
+        {
+            return Head == null ? String.Empty : Head.ToString();
+        }
+    }
+}
--- Gyakorlo5/Program.cs
+++ Gyakorlo5/Program.cs
@@ -1,8 +1,45 @@
-﻿
-public class Gyakorlo5
+﻿namespace MyLinkedList
 {
-    public static void Main(string[] args)
+    public class Gyakorlo5
     {
-
+        public static void Main(string[] args)
+        {
+            MyLinkedList ml = new MyLinkedList();
+            Console.WriteLine("AddLast 58: " + ml.AddLast(58) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddFirst 12: " + ml.AddFirst(12) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddFirst 58: " + ml.AddFirst(58) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddFirst 39: " + ml.AddFirst(39) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddFirst 47: " + ml.AddFirst(47) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddFirst 58: " + ml.AddFirst(58) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddLast 17: " + ml.AddLast(17) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddFirst 26: " + ml.AddFirst(26) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddLast 58: " + ml.AddLast(58) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddFirst 95: " + ml.AddFirst(95) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddLast 31: " + ml.AddLast(31) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddLast 58: " + ml.AddLast(58) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddLast 58: " + ml.AddLast(58) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddLast 88: " + ml.AddLast(88) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddFirst 44: " + ml.AddFirst(44) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddLast 63: " + ml.AddLast(63) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddLast 58: " + ml.AddLast(58) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("AddFirst 58: " + ml.AddFirst(58) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("contains 44: " + ml.Contains(44));
+            Console.WriteLine("contains 45: " + ml.Contains(45));
+            Console.WriteLine("removeFirstOccurence 95: " + ml.RemoveFirstOccurrence(95) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("removeAll 58: " + ml.RemoveAll(58) + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("removeLast: " + ml.RemoveLast() + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("removeFirst: " + ml.RemoveFirst() + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("removeFirst: " + ml.RemoveFirst() + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("removeLast: " + ml.RemoveLast() + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("removeFirst: " + ml.RemoveFirst() + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("removeFirst: " + ml.RemoveFirst() + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("removeLast: " + ml.RemoveLast() + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("removeLast: " + ml.RemoveLast() + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("removeFirst: " + ml.RemoveFirst() + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("removeLast: " + ml.RemoveLast() + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("removeFirst: " + ml.RemoveFirst() + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("removeFirst: " + ml.RemoveFirst() + " " + ml + " size: " + ml.Size);
+            Console.WriteLine("removeLast: " + ml.RemoveLast() + " " + ml + " size: " + ml.Size);
+        }
     }
 }
